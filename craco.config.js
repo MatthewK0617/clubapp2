3eb29b53ca6de727b68aa9b88c6332b638ac1ec7
Matthew Kim <patmattkim@gmail.com>
2021-09-16 09:29:15 -0700

    created postcss config file

--- craco.config.js
+++ craco.config.js
@@ -1,6 +1,4 @@
 // craco.config.js
-const purgecss = require('@fullhuman/postcss-purgecss')
-const cssnano = require('cssnano')
 
 module.exports = {
     style: {
@@ -8,13 +6,6 @@ module.exports = {
         plugins: [
           require('tailwindcss'),
           require('autoprefixer'),
-          cssnano({
-            preset: 'default'
-          }),
-          purgecss({
-            content: ['./public/*.html'],
-            defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
-          })
         ],
       },
     },
